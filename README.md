12f9cc49fdb25aab847c2d83e4948583c3152913
ctodd03 <ctodd03@uoguelph.ca>
2022-01-23 16:45:37 -0500

    updated readme

--- README.md
+++ README.md
@@ -10,19 +10,38 @@ The webscraper we created scrapes data from the University of Guelph course desc
 and saves the data of all courses at UofG into a JSON file. The data is in the format: course code, course name, course description, semesters offered, lecture times, 
 restrictions, offerings, credit weight, department, location, prerequesites and equates.
 
+The CLI we created allows the user to search courses by
+
 ### Prerequisites
 
--NodeJS (npm)
--Python 3
--PlayWright
+-NodeJS (https://nodejs.dev/)
+-Python 3 (https://www.python.org/downloads/)
+-PlayWright (https://playwright.dev/)
 
 ### Installation
 
+To install project simply download the project folder to a compatible system.
+
 Install PlayWright:
+1. "npx playwright install" in shell.
+
 
 ## Usage
 
-Example command:
+To use the webscraper:
+
+1. Navigate to "scraper" folder.
+2. Type "npx run scraper.js"
+
+The scraper will then create a file named "data.json" containing all the 
+course data form the UofG course descriptions website.
+
+To use the CLI tool:
+
+1. Ensure the "data.json" file has been created and is in the "scaper" folder.
+2. Navigate to root project folder.
+3. Type "python3 dataReader.py"
+4. Use the numbered menu to operate the CLI tool and follow the prompts to perform searchs.
 
 
 
--- scraper/tests/scraper.spec.js
+++ scraper/tests/scraper.spec.js
@@ -2,8 +2,50 @@
 // test for scraper.js
 const { test, expect } = require('@playwright/test');
 
-test('scraper', async ({ page }) => {
+test.use({
+  viewport: {
+    width: 800,
+    height: 600
+  },
+  colorScheme: 'dark'
+});
+
+//tests opening the guelph course descriptions page
+test('main page', async ({ page }) => {
+  //open page
   await page.goto('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/');
   //await page.locator('text=Get started').click();
-  await expect(page).toHaveTitle(/2021-2022 Academic Calendar/);
+  await expect(page).toHaveTitle("XII. Course Descriptions < University of Guelph");
 });
+
+//tests opening a major within the guelph course descriptions page
+test('course page', async ({ page }) => {
+
+  await page.goto('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/acct/');
+  //await page.locator('text=Get started').click();
+  await expect(page).toHaveTitle("Accounting (ACCT) < University of Guelph");
+});
+
+test('initBrowser', async ({ page }) => {
+  //open page
+  await page.goto('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/');
+  await expect(page).toHaveTitle("XII. Course Descriptions < University of Guelph");
+});
+
+// test('test', async ({ page }) => {
+//   // Go to https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/
+//   await page.goto('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/');
+//   // Click [aria-label="Primary"] >> text=Accounting (ACCT)
+//   await page.click('[aria-label="Primary"] >> text=Accounting (ACCT)');
+//   await expect(page).toHaveURL('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/acct/');
+//   // Click [aria-label="Primary"] >> text=Agriculture (AGR)
+//   await page.click('[aria-label="Primary"] >> text=Agriculture (AGR)');
+//   await expect(page).toHaveURL('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/agr/');
+//   // Click [aria-label="Primary"] >> text=Animal Science (ANSC)
+//   await page.click('[aria-label="Primary"] >> text=Animal Science (ANSC)');
+//   await expect(page).toHaveURL('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/ansc/');
+//   // Click [aria-label="Primary"] >> text=Zoology (ZOO)
+//   await page.click('[aria-label="Primary"] >> text=Zoology (ZOO)');
+//   await expect(page).toHaveURL('https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/zoo/');
+// });
+
