49599aa118ffbda0296cd0f19f67692d00bd7322
dlakhiani <dlakhiani02@gmail.com>
2022-01-25 15:36:35 -0500

    mapped major to course and prereqs

--- course_graphs/generate_graph.py
+++ course_graphs/generate_graph.py
@@ -4,7 +4,7 @@ def readJSON(file="scraper/data.json"):
     """open file and load json data into 'data'
 
     Args:
-        file ([String]): name of file to read in and parse JSON
+        file ([String]): [name of file to read in and parse JSON]
     """
     with open(file, "r") as f:
         data = json.load(f)
@@ -12,15 +12,46 @@ def readJSON(file="scraper/data.json"):
     return data
 
 def getMajorCode(major):
+    """parse major code from 'major' key in JSON
+
+    Args:
+        major ([String]): [name of major to parse code from]
+
+    Returns:
+        [String]: [major code]
+    """
     return major[major.find('(')+1:-1]
+
+def mapCoursesPrereqs(courses):
+    """map each course in given major to prerequisites of the course
+
+    Args:
+        courses ([List]): [json dicts of all courses in major]
+
+    Returns:
+        [Dictionary]: [course code mapped to prerequisites]
+    """
+    prereqDict = {course['cCode'] : course['prereqs'] for course in courses}
+    return prereqDict
+
+def mapMajorCourses(majors):
+    """map major's code to courses (with prereqs mapped) supplied in major
+
+    Args:
+        majors ([List]): [json dicts of all available majors]
+
+    Returns:
+        [Dictionary]: [major's code mapped to courses]
+    """
+    majorDict = {getMajorCode(major['major']) : mapCoursesPrereqs(major['title']) for major in majors}        
+    return majorDict
 
 def main():
     data = readJSON()
-
-    majorDict = {}
-    for major in data:
-        #map major's course code to courses supplied in major
-        majorDict[getMajorCode(major['major'])] = major['title']
+    majorDict = mapMajorCourses(data)
+    
+    with open("course_graphs/prereqs.json", "w") as f:
+        json.dump(majorDict, f)
 
 if __name__ == '__main__':
-    main()
+    main()  
