8996e62828faea3b2886d5c8c294d778eeaf3698
zbelgrav <zbelgrav@uoguelph.ca>
2022-01-30 18:28:53 -0500

    added unit testing

---
+++ courseGraphFunctions/testFunctions.py
@@ -0,0 +1,189 @@
+import unittest
+
+import json
+
+import courseGraph as cg
+import graphFunctions as gp
+
+
+sampleData = [
+    {
+        "major": "Accounting (ACCT)",
+        "title": [
+            {
+                "cCode":"ACCT*1220",
+                "name":"Introductory Financial Accounting",
+                "description":"This introductory course is designed to develop a foundational understanding of current accounting principles and their implication for published financial reports of business enterprises. It builds the base of knowledge and understanding required to succeed in more advanced study of accounting. The course approaches the subject from the point of view of the user of accounting information rather than that of a person who supplies the information.",
+                "semesters":"Fall and Winter",
+                "lec":"LEC: 3",
+                "restrictions":"ACCT*2220. This is a Priority Access Course. Enrolment may be restricted to particular programs or specializations. See department for more information.",
+                "offerings":"Also offered through Distance Education format.",
+                "creditWeight":"[0.50]",
+                "department":"Department of Management",
+                "location":"Guelph",
+                "prereqs":None,
+                "coreqs":None,
+                "equates":None
+            },
+            {
+                "cCode":"ACCT*1240",
+                "name":"Applied Financial Accounting",
+                "description":"This course requires students to apply the fundamental principles emanating from accounting's conceptual framework and undertake the practice of financial accounting. Students will become adept at performing the functions related to each step in the accounting cycle, up to and including the preparation of the financial statements and client reports. Students will also develop the skills necessary for assessing an organization's system of internal controls and financial conditions.",
+                "semesters":"Winter Only",
+                "lec":"LEC: 3",
+                "restrictions":"ACCT*2240. This is a Priority Access Course. Enrolment may be restricted to particular programs or specializations. See department for more information.",
+                "offerings":"Also offered through Distance Education format.",
+                "creditWeight":"[0.50]",
+                "department":"Department of Management",
+                "location":"Guelph",
+                "prereqs":"ACCT*1220 or ACCT*2220",
+                "coreqs":None,
+                "equates":None
+            },
+            {
+                "cCode":"ACCT*2230",
+                "name":"Management Accounting",
+                "description":"This course emphasizes the use of accounting information to facilitate effective management decisions. Topics include cost determination, cost control and analysis, budgeting, profit-volume analysis and capital investment analysis.",
+                "semesters":"Fall and Winter",
+                "lec":"LEC: 3",
+                "restrictions":"This is a Priority Access Course. Enrolment may be restricted to particular programs or specializations. See department for more information.",
+                "offerings":None,
+                "creditWeight":"[0.50]",
+                "department":"Department of Management",
+                "location":"Guelph",
+                "prereqs":"ACCT*1220 or ACCT*2220",
+                "coreqs":None,
+                "equates":"AGEC*2230, BUS*2230"
+            },
+            {
+                "cCode":"ACCT*3230",
+                "name":"Intermediate Management Accounting",
+                "description":"This course continues the managerial decision making focus of ACCT*2230. Topics include process costing, transfer pricing, the decision making process, variances and performance measurement.",
+                "semesters":"Winter and Summer",
+                "lec":"LEC: 3",
+                "restrictions":"Enrolment may be restricted to particular degrees or programs. See department for more information.",
+                "offerings":"Also offered through Distance Education format.",
+                "creditWeight":"[0.50]",
+                "department":"Department of Management",
+                "location":"Guelph",
+                "prereqs":"ACCT*2230",
+                "coreqs":None,
+                "equates":"BUS*3230"
+            }
+        ]
+    },
+    {
+        "major": "Chemistry (CHEM)",
+        "title": [
+            {
+                "cCode":"CHEM*1040",
+                "name":"General Chemistry I",
+                "description":"This course introduces concepts of chemistry, the central link between the physical and biological sciences. Principles discussed include chemical bonding, simple reactions and stoichiometry, chemical equilibria and solution equilibria (acids, bases, and buffers), and introductory organic chemistry.",
+                "semesters":"Fall and Winter",
+                "lec":"LEC: 3, LAB: 3",
+                "restrictions":None,
+                "offerings":None,
+                "creditWeight":"[0.50]",
+                "department":"Department of Chemistry",
+                "location":"Guelph",
+                "prereqs":"4U Chemistry (or equivalent) or CHEM*1060",
+                "coreqs":None,
+                "equates":None
+            },
+            {
+                "cCode":"CHEM*1050",
+                "name":"General Chemistry II",
+                "description":"This course provides an introductory study of the fundamental principles governing chemical transformations: thermodynamics (energy, enthalpy, and entropy); kinetics (the study of rates of reactions); and redox/electrochemistry.",
+                "semesters":"Fall and Winter",
+                "lec":"LEC: 3, LAB: 3",
+                "restrictions":None,
+                "offerings":None,
+                "creditWeight":"[0.50]",
+                "department":"Department of Chemistry",
+                "location":"Guelph",
+                "prereqs":"CHEM*1040",
+                "coreqs":None,
+                "equates":None
+            },
+            {
+                "cCode":"CHEM*2060",
+                "name":"Structure and Bonding",
+                "description":"This course covers the applications of symmetry, simple crystal structures and principles of bonding. Molecular orbital theory is used to explain the fundamental relationship between electronic and molecular structure. This course provides the elementary quantum background for an understanding of the electronic structures of atoms and molecules.",
+                "semesters":"Fall Only",
+                "lec":"LEC: 3, LAB: 1.5",
+                "restrictions":None,
+                "offerings":None,
+                "creditWeight":"[0.50]",
+                "department":"Department of Chemistry",
+                "location":"Guelph",
+                "prereqs":"CHEM*1050, [IPS*1510 or (MATH*1210, (1 of PHYS*1010, PHYS*1070, PHYS*1300))]",
+                "coreqs":None,
+                "equates":None
+            },
+            {
+                "cCode":"CHEM*2070",
+                "name":"Structure and Spectroscopy",
+                "description":"This course provides an introduction to spectroscopy and its relationship to molecular structure and dynamics. Rotational, vibrational, electronic and magnetic resonance spectroscopies will be studied. Concepts introduced in CHEM*2060 will be applied to chemical and biochemical problems through spectroscopic techniques. Central to this course is the use of spectroscopy for the determination of molecular structures and the investigation of molecular motions.",
+                "semesters":"Winter and Summer",
+                "lec":"LEC: 3, LAB: 1.5",
+                "restrictions":None,
+                "offerings":None,
+                "creditWeight":"[0.50]",
+                "department":"Department of Chemistry",
+                "location":"Guelph",
+                "prereqs":"CHEM*2060",
+                "coreqs":None,
+                "equates":None
+            }
+        ]
+    }
+]
+
+
+class testCLI(unittest.TestCase):
+    #possibly test readJSON()
+    def test_getMajorCode(self):
+        res = cg.getMajorCode(sampleData[0]['major'])
+        expected = "ACCT"
+        self.assertEqual(res, expected, "Failed courseGraph.<getMajorCode>\n")
+
+    def test_mapCoursesInfo(self):
+        res = cg.mapCoursesInfo(sampleData[0]['title'])
+
+        expected = {
+                "prereqs":"ACCT*2230",
+                "coreqs":None,
+                "equates":"BUS*3230"
+                }
+        self.assertEqual(res['ACCT*3230'], expected, "Failed courseGraph.<mapCoursesInfo>")
+
+
+    def test_mapMajorCourses(self):
+        #print(sampleData[0])
+        
+        expected = {'ACCT': {'prereqs': None, 'coreqs': None, 'equates': None},
+
+                }
+
+        print(cg.mapMajorCourses(sampleData))
+
+    def test_displayGraph(self):
+        flag = 1
+
+    def test_drawGraph(self):
+        flag = 1
+
+    def test_parseReqs(self):
+        flag = 1
+
+    def test_generateGraphByMajor(self):
+        flag = 1
+
+    def test_generateGraphByCourse(self):
+        flag = 1
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
