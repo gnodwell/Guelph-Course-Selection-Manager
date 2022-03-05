43708a6826637b8b6eda45a59851052df47feea6
Zadyn <zadyn@debian-BULLSEYE-live-builder-AMD64>
2022-03-05 10:16:19 -0500

    fixed index.js

--- front/src/index.js
+++ front/src/index.js
@@ -11,7 +11,6 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  console.log("HEY")
 );
 
 // If you want to start measuring performance in your app, pass a function
