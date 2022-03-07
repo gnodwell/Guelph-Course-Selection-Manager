15d43a85646c026568c5b48c73d6979cef8956bb
zbelgrav <zbelgrav@uoguelph.ca>
2022-03-06 22:47:06 -0500

    updated installation script

--- installationScripts/webAppSetup.sh
+++ installationScripts/webAppSetup.sh
@@ -1,3 +1,18 @@
-echo "What is your name?"
-read PERSON
-echo "Hello, $PERSON"
+echo "Starting installations"
+
+npm install jquery
+
+npm install -g create-react-app
+
+pip install -U Flask
+
+
+sudo apt update
+sudo apt install nginx
+sudo ufw allow 'Nginx HTTP'		
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt ## OpenSSL self-signed key and certificate pair
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048   ## generate Diffie-Hellman group
+sudo ufw app list       ## adjust firewall
+
+
+
