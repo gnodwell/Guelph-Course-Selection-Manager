5b97db73b5e428501a5d5db81a6885e8bca25fdb
zbelgrav <zbelgrav@uoguelph.ca>
2022-03-09 17:26:50 -0500

    Added navbar

--- webapp/front/src/App.js
+++ webapp/front/src/App.js
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AppBar>
+          <Toolbar>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6'>
+              CIS*3760 Team 4
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <img src={logo} className="App-logo" alt="logo" />
 
         <Button onClick = {()=>alert('Hello World!')} variant='contained' color='primary'>
           Hello World
         </Button>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
