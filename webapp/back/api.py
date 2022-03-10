8a2a02545d044c49869ee11513f6d39def5d8753
alanren0 <68366679+alanren0@users.noreply.github.com>
2022-03-09 21:00:13 -0500

    Started basic flask api. Runs locally

---
+++ webapp/back/api.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+    
+
+@app.route('/', methods=['GET'])
+def filter():
+    args = request.args
+    filters = {}
+    filters['creditWeight'] = args.get('creditWeight')
+    filters['semesters'] = args.get('semesters')
+
+
+    with open('data.json', 'r') as f:
+        res = []
+        data = json.load(f)
+
+        #loop through department
+        for department in data:
+            #loop through courses in department
+            for course in department['title']:
+                add = True
+                #loop through filters
+                for k, v in filters.items():
+                    
+                    #if course does not match the filter value, don't add course
+                    if v != None and v not in (course[k]):
+                        add = False
+                        break
+                
+                #add course if it passed filters
+                if add:
+                    res.append(course)
+
+    
+        return json.dumps(res)
+
+
+if __name__ == '__main__':
+    app.run()
