127046d51a67f6842819df48bb091c0ebcbb5939
alanren0 <68366679+alanren0@users.noreply.github.com>
2022-03-11 17:19:49 -0500

    fix merge conflict and update fetch and api

--- webapp/back/api.py
+++ webapp/back/api.py
@@ -2,16 +2,23 @@ from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
-
     
 
 @app.route('/', methods=['GET'])
 def filter():
     args = request.args
     filters = {}
-    filters['creditWeight'] = args.get('creditWeight')
+    filters['cCode'] = args.get('cCode')
+    filters['name'] = args.get('name')
     filters['semesters'] = args.get('semesters')
-
+    filters['restrictions'] = args.get('restrictions')
+    filters['offerings'] = args.get('offerings')
+    filters['creditWeight'] = args.get('creditWeight')
+    filters['department'] = args.get('department')
+    filters['location'] = args.get('location')
+    filters['prereqs'] = args.get('prereqs')
+    filters['coreqs'] = args.get('coreqs')
+    
 
     with open('data.json', 'r') as f:
         res = []
@@ -24,9 +31,9 @@ def filter():
                 add = True
                 #loop through filters
                 for k, v in filters.items():
-                    
                     #if course does not match the filter value, don't add course
-                    if v != None and v not in (course[k]):
+                    #checks if the filter exists and the course doesn't have a value for that filter or if the filter exists but the course dosn't match
+                    if (v and not course[k]) or (v and v.lower() not in course[k].lower()):
                         add = False
                         break
                 
--- webapp/front/src/Pages/Home.js
+++ webapp/front/src/Pages/Home.js
@@ -1,39 +1,44 @@
 import Button from '@material-ui/core/Button'
 import logo from '../logo.svg';
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 
-function home() {
-    // const [courses, setCourses] = useState([])
-    // const fetchCourses = async() => {
-    //     fetch('131.104.49.104:5000', {
-    //         'methods': 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     })
-    //     .then(response => {
-    //         setCourses(response.json())
-    //         console.log(courses)})
-    //     .catch(error => console.log(error))
-    // };
+function Home() {
+    const [courses, setCourses] = useState([])
+
+    const fetchCourses = async() => {
+        fetch('http://131.104.49.104:5000/', {
+            'methods': 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => setCourses(data))
+        .catch(error => console.log(error))
+    };
     return (
         <div className="homePage">
             <h2>Home Page</h2>
 
             <img src={logo} className="App-logo" alt="logo" />
 
-            {/* <Button onClick = {fetchCourses} variant='contained' color='primary'>
-            Fetch Courses
-            </Button> */}
 
-            
+        <Button onClick = {fetchCourses} variant='contained' color='primary'>
+            Fetch Courses
+        </Button>
+        
+        {courses && <div>{
+          courses.map(course => (
+            <p key={course.cCode}>course code: {course.cCode}</p>
+        ))}
+        </div>}
         
         </div>
     );
 }
 
-export default home;
+export default Home;
 
