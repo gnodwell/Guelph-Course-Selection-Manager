26af2cce43e7c12dcc57c551dacdc781031d5ab3
Gavin Nodwell <gavinnodwell@Gavins-Air.hitronhub.home>
2022-03-27 23:25:09 -0400

    Trying to fail python test

--- webapp/back/createMajorGraphs.test.py
+++ webapp/back/createMajorGraphs.test.py
@@ -5,7 +5,7 @@ import createMajorGraphs
 class testCreateMajorGraph(unittest.TestCase):
     def test_getBaseCode(self):
         res = createMajorGraphs.getBaseCode("CIS*3760")
-        self.assertEqual("CIS", res,"Failed createMajorGraphs.<getBaseCode>")
+        self.assertEqual("CISS", res,"Failed createMajorGraphs.<getBaseCode>")
 
 if __name__ == '__main__':
     unittest.main()
