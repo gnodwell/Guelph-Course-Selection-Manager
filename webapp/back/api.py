59d39591b5aa64e135dfb0916415fed3b69dc274
Gavin Nodwell <gavinnodwell@Gavins-MacBook-Air.local>
2022-04-04 11:18:49 -0400

    pushing

--- webapp/back/api.py
+++ webapp/back/api.py
@@ -162,6 +162,6 @@ def dropCourseGraph():
 
 if __name__ == '__main__':
      context = ('/etc/ssl/certs/nginx-selfsigned.crt','/etc/ssl/private/nginx-selfsigned.key')
-    # # app.run(host='131.104.49.104', debug=True, ssl_context=context)
+    # # # app.run(host='131.104.49.104', debug=True, ssl_context=context)
      app.run(host='0.0.0.0', debug=False, ssl_context=context)
     # app.run()
